6694f69f5e7bf487f8099430d00c7285a4076951
Jean Carlos Olivarez <Jean.Olivarez@hit.com.do>
2017-05-11 17:03:12 -0400

    4

---
+++ quickstart/src/app/Component/Login/Login.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: "login",
+    templateUrl: "../../../../Views/Login/index.html",
+    styleUrls: [
+        "../../../../assets/css/login.css",
+        
+    ]
+
+})
+
+export class LoginComponet {
+    title = "Login";
+
+}
---
+++ quickstart/src/app/Module/app-routing.module.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var Login_1 = require("../Component/Login/Login");
+var routes = [
+    { path: '', redirectTo: '/Login', pathMatch: 'full' },
+    { path: 'Login', component: Login_1.LoginComponet }
+];
+var AppRoutingModule = (function () {
+    function AppRoutingModule() {
+    }
+    return AppRoutingModule;
+}());
+AppRoutingModule = __decorate([
+    core_1.NgModule({
+        imports: [router_1.RouterModule.forRoot(routes)],
+        exports: [router_1.RouterModule]
+    })
+], AppRoutingModule);
+exports.AppRoutingModule = AppRoutingModule;
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=app-routing.module.js.map
