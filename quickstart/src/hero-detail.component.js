bfeda17bba36774f246f3d8768851f3eddb57891
Jean Carlos Olivarez <Jean.Olivarez@hit.com.do>
2017-05-15 13:29:34 -0400

    deleting all files

--- quickstart/src/hero-detail.component.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=hero-detail.component.js.map
--- quickstart/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/Module/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
